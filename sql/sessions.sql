bde4d1799dc19ce72769a987e761712afb512d8f
Flemming Andersen <flemming@flamingcode.com>
2013-08-17 16:08:36 +0200

    Changed db structure to generic collation (utf8_general_ci)

--- sql/sessions.sql
+++ sql/sessions.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `sessions` (
-  `id` char(32) CHARACTER SET utf8 COLLATE utf8_danish_ci NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_danish_ci NOT NULL,
+  `id` char(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `modified` int(11) DEFAULT NULL,
   `lifetime` int(11) DEFAULT NULL,
-  `data` text CHARACTER SET utf8 COLLATE utf8_danish_ci,
+  `data` text CHARACTER SET utf8 COLLATE utf8_general_ci,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
